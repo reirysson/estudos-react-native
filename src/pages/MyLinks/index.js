5dd01c9e225eea9e976db1946a62a6b30880d124
reirysson <reiryssonjp@gmail.com>
2022-06-21 22:20:45 -0300

    começando a navegação

--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function MyLinks() {
     return(
         <View>
-            <Text>Pagina links</Text>
+            <Text>Pagina MyLinks</Text>
         </View>
     )
 }
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Home from './pages/Home';
+import MyLinks from './pages/MyLinks';
+
+const Drawer = createDrawerNavigator();
+
+function Routes() {
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen
+                
+            />
+        </Drawer.Navigator>
+    )
+}
