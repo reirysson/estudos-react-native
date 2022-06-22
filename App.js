c0dfdbf7f3fd47cc698f95272f5493ae412909ed
reirysson <reiryssonjp@gmail.com>
2022-06-21 21:43:48 -0300

    iniciando a criação de componentes

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Que top</Text>
-      <StatusBar style="auto" />
+  return(
+    <View >
+      <Text>Texto teste</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
