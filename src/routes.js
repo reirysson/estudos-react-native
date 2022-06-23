db7bd432dc133164ea5132399a6e4727bc098950
reirysson <reiryssonjp@gmail.com>
2022-06-23 18:49:33 -0300

    iniciando a estilizacao do drawer

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,13 @@ const Drawer = createDrawerNavigator();
 
 function Routes() {
     return(
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            screenOptions={{
+                drawerActiveBackgroundColor: '#2CCBB9',
+                drawerActiveTintColor: '#FFF',
+                drawerMarginTop: 16,
+            }}
+        >
             <Drawer.Screen
                 name="Home"
                 component={Home}
